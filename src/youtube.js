44663694ca1af0e89a8f7790d1a5080ef60b3480
Wen Lee <130221926+leeeweee1710@users.noreply.github.com>
2023-08-23 01:50:25 +0800

    fifty

--- src/youtube.js
+++ src/youtube.js
@@ -20,24 +20,22 @@ export async function getVideoIDsFromPlaylist(apiKey, playlistId, pageToken) {
   return videos;
 }
 
-export async function getVideosByID(apiKey, videoIds, pageToken) {
-  const response = await fetch(
-    `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&id=${videoIds}&maxResults=50${
-      pageToken ? '&pageToken=' + pageToken : ''
-    }&key=${apiKey}`,
-    {
-      headers: {
-        // Authorization: 'Bearer ',
-        Accept: 'application/json',
-      },
-    }
-  );
-  const data = await response.json();
-  const videos = data.items;
-  if (data.nextPageToken) {
-    return videos.concat(
-      await getVideosByID(apiKey, videoIds, data.nextPageToken)
+export async function getVideosByID(apiKey, videoIds) {
+  const videos = [];
+  for (let i = 0; i < videoIds.length; i += 50) {
+    // you can only get 50 videos at a time
+    const chunk = videoIds.slice(i, i + 50);
+    const response = await fetch(
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&id=${chunk}&key=${apiKey}`,
+      {
+        headers: {
+          // Authorization: 'Bearer ',
+          Accept: 'application/json',
+        },
+      }
     );
+    const data = await response.json();
+    videos.push(...data.items); // modify the array
   }
   return videos;
 }
